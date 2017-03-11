e259335e4083de5b3f93a053e4417d20838f2169
Mike Wolf <mw@mike-wolf.com>
2017-03-11 15:16:02 -0500

    Moved to HTML loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,33 @@
 // webpack and webpack-hot-middleware documentation
 var webpack = require('webpack');
 var path = require('path');
-
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   // debug: true,
   devtool: '#eval-source-map',
-  context: path.join(__dirname, 'app', 'js'),
+  // context: path.join(__dirname, 'app', 'js'),
 
   entry: [
     'webpack/hot/dev-server',
     'webpack-hot-middleware/client',
-    './main'
+    path.join(__dirname, 'app/js/main')
   ],
   stats: {
     chunks: false,
   },
   output: {
     path: path.join(__dirname, 'app', 'js'),
-    publicPath: '/js/',
+    publicPath: '/',
     filename: 'bundle.js'
   },
 
   plugins: [
+    new HtmlWebpackPlugin({
+          template: 'app/index.tpl.html',
+          inject: 'body',
+          filename: 'index.html'
+        }),
+    // new
     // new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
