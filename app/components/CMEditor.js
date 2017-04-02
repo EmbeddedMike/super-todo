07a800533d4db117331392f5a767c6be551f23f6
Mike Wolf <mw@mike-wolf.com>
2017-04-02 11:50:02 -0400

    Convert CMEditor to class

--- app/components/CMEditor.js
+++ app/components/CMEditor.js
@@ -1,24 +1,24 @@
 const React = require('react');
 const CodeMirror = require('react-codemirror');
 require('codemirror/lib/codemirror.css');
-const CMEditor = React.createClass({
-	getInitialState: function() {
-		return {
-			code: "// Codexxx",
-		};
-	},
-	updateCode: function(newCode) {
+class CMEditor extends React.Component{
+	constructor(props) {
+		super(props);
+		this.state = {code: "//Test"}
+		
+	}
+	updateCode(newCode) {
 		this.setState({
 			code: newCode,
 		});
-	},
-	render: function() {
+	}
+	render() {
 		var options = {
 			lineNumbers: true,
 		};
-		return <CodeMirror value={this.state.code} onChange={this.updateCode} options={options} />
+		return <CodeMirror value={this.state.code} onChange={this.updateCode.bind(this)} options={options} />
 	}
-});
+};
 
 
 export default CMEditor;
