298c7d2fa1db8bab831c1425f8ccd158d4d4ac2c
Mike Wolf <mw@mike-wolf.com>
2017-04-02 23:12:50 -0400

    Added persist to local storage

--- app/store/configureStore.dev.js
+++ app/store/configureStore.dev.js
@@ -1,12 +1,21 @@
-import { createStore } from 'redux';
 import rootReducer from '../reducers';
 import DevTools from '../containers/DevTools';
+import {compose, createStore} from 'redux';
+import persistState from 'redux-localstorage'
+const config = {
+    key: "todo"
+};
+const enhancer = compose(
+  DevTools.instrument(),
+  persistState(undefined, config),
+)
 
+// const store = createStore(/*reducer, [initialState]*/, enhancer)
 export default function configureStore(initialState) {
     const store = createStore(
         rootReducer,
         initialState,
-        DevTools.instrument()
+        enhancer
     );
 
     return store;
