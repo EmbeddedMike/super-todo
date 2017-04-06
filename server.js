315568ffc4ea110ac913abcd6328e7582ece7d90
Mike Wolf <mw@mike-wolf.com>
2017-04-06 15:39:31 -0400

    refactored to modularize callbacks

--- server.js
+++ server.js
@@ -24,29 +24,29 @@ var throttle = require('throttle-debounce/throttle');
 /**
  * Run Browsersync and use middleware for Hot Module Replacement
  */
- var bs = browserSync.create()
- const allMiddleware = (req,res,next) => {
-  require("./server/all.js")(bs,req,res,next)
+var bs = browserSync.create()
+const allMiddleware = (req, res, next) => {
+  require("./server/all.js")(bs, req, res, next)
 }
 
 const chokidar = require('chokidar');
 const watcher = chokidar.watch('./server');
 let clearCache = (mode, id) => {
   console.log("Clearing /server/ module cache from server :" + id);
-  const resolved = require.resolve("./" +id);
+  const resolved = require.resolve("./" + id);
   let module;
   let state;
-  if (require.cache[resolved]){
+  if (require.cache[resolved]) {
     module = require(resolved);
-    if (module.deregister ) state = module.deregister()
+    if (module.deregister) state = module.deregister()
     delete require.cache[resolved];
   }
   state = state ? state : {}
-  try{
+  try {
     module = require(resolved);
-    if (module.register ) module.register(bs, state)
+    if (module.register) module.register(bs, state)
   }
-  catch (e){
+  catch (e) {
     console.log(e)
   }
 
@@ -54,45 +54,45 @@ let clearCache = (mode, id) => {
 
 const delay = process.env.PROJECT_NAME ? 2000 : 20
 
-clearCache = throttle(delay, false, clearCache );
+clearCache = throttle(delay, false, clearCache);
 
-watcher.on('ready', function() {
+watcher.on('ready', function () {
   watcher.on('all', clearCache);
 });
 
 
 var bsConfig = {
-    server: {
-      baseDir: 'app',
-      ws: true,
-      middleware: [
-        webpackDevMiddleware(bundler, {
-          // IMPORTANT: dev middleware can't access config, so we should
-          // provide publicPath by ourselves
-          publicPath: webpackConfig.output.publicPath,
+  server: {
+    baseDir: 'app',
+    ws: true,
+    middleware: [
+      webpackDevMiddleware(bundler, {
+        // IMPORTANT: dev middleware can't access config, so we should
+        // provide publicPath by ourselves
+        publicPath: webpackConfig.output.publicPath,
 
-          // pretty colored output
-          stats: { chunks: false }
+        // pretty colored output
+        stats: { chunks: false }
 
-          // for other settings see
-          // http://webpack.github.io/docs/webpack-dev-middleware.html
-        }),
+        // for other settings see
+        // http://webpack.github.io/docs/webpack-dev-middleware.html
+      }),
 
-        // bundler should be the same as above
-        webpackHotMiddleware(bundler),
-        //this is a hot reloaded middleware
-        allMiddleware
+      // bundler should be the same as above
+      webpackHotMiddleware(bundler),
+      //this is a hot reloaded middleware
+      allMiddleware
 
 
-      ]
-    },
+    ]
+  },
 
-    // no need to watch '*.js' here, webpack will take care of it for us,
-    // including full page reloads if HMR won't work
-    // files: [
-    //   'app/css/*.css',
-    //   'app/*.html'
-    // ]
+  // no need to watch '*.js' here, webpack will take care of it for us,
+  // including full page reloads if HMR won't work
+  // files: [
+  //   'app/css/*.css',
+  //   'app/*.html'
+  // ]
 
 
 };
@@ -103,32 +103,44 @@ const readTodoList = (name) => {
   return fsp.readFile(path.join(".data", name + ".md")).then(contents => contents.toString());
 }
 const socketCode = () => {
-  require("./server/socket.js")(bs,req,res,next)
-  }
-const initted = function() {
+  require("./server/socket.js")(bs, req, res, next)
+}
+const initted = function () {
   // socketCode()
   console.log("Initializing");
-  state = {}
+  let state = {}
   state.seq = 0;
-  let connections = {}
   var sock = bs.sockets;
-  sock.on('connection', function (socket) {
+  state.assignID = (socket) => {
     state.seq++;
-    let id = "Session " + state.seq
+    id = "Session " + state.seq
     console.log(`Session ${id} connected~~~~`)
     socket.send("id", id);
-    socket.on('message', function (type, body) {
-      // we tell the client to execute 'new message'
-      console.log("got a message " + type);
-      if( type === "getTodo"){
-        socket.send("id", id);
-        readTodoList("mwolf").then(
-          list => socket.send("todo","THIS IS THE list" ));
-      }
+    return id;
+  }
+  state.processMessage = (socket, id, type, body) => {
+    // we tell the client to execute 'new message'
+    console.log("got a message " + type);
+    if (type === "getTodo") {
+      socket.send("id", id);
+      readTodoList("mwolf").then(
+        list => socket.send( "todo",
+          {
+            id: id,
+            todo: "THIS IS THE list"
+          }));
+    }
+  }
+
+  if (!state.onConnection) {
+    state.onConnection = sock.on('connection', (socket) => {
+      let id = state.assignID(socket)
+      socket.on('message', (type, body) => {
+        state.processMessage(socket, id, type, body);
       });
-
-  });
+    });
+  }
 };
-  // sock.on ("connect")
+// sock.on ("connect")
 
-bs.init(bsConfig,initted);
+bs.init(bsConfig, initted);
