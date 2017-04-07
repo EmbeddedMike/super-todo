c498d37b230be8c9d6b52154d6fa8325980a95b5
Mike Wolf <mw@mike-wolf.com>
2017-04-06 20:25:28 -0400

    Refactored and cleaned up socket plugin

--- server/socket.js
+++ server/socket.js
@@ -1,37 +1,14 @@
 console.log("socket js")
-module.exports = () =>{
-  console.log("Socket.js called")
-}
 
+module.exports = {} //structure to hang register and deregister on
+
+//These will be set inside a function and need to be hoisted to module level
 let state = {}
-let socket = null;
-let doAssignments = () =>{
-  state.assignID = (socket) => {
-    state.seq++;
-    id = "QUACK " + state.seq
-    console.log(`Session ${id} connected~~~~`)
-    socket.send("id", id);
-    return id;
-  }
-  state.processMessage = (socket, id, type, body) => {
-    // we tell the client to execute 'new message'
-    console.log("got a message " + type);
-    if (type === "getTodo") {
-      socket.send("id", id);
-      readTodoList("mwolf").then(
-        list => socket.send("todo",
-          {
-            id: id,
-            todo: "THIS IS NOT THE list"
-          }));
-    }
-  }
-}
-const path = require("path");
-const fsp = require("fs-promise");
 
-const readTodoList = (name) => {
-  return fsp.readFile(path.join(".data", name + ".md")).then(contents => contents.toString());
+//The deregister function exports the state so it can be 
+//forwarded to the reloaded module
+module.exports.deregister = () => {
+  return state;
 }
 
 module.exports.register = (bs, newState) => {
@@ -39,31 +16,66 @@ module.exports.register = (bs, newState) => {
   sock = bs.sockets
   if (newState) {
     state = newState;
-    console.log(state.connectionData.length + " connections")
-    state.connectionData.forEach( (element,i) => {
-      console.log( `${i}: ${element.id}`)
-    }, this);
-    doAssignments();
+    setupState()
+    processEntriesFrom(0);
   } else {
     console.log("Setting up");
-    state = {}
-    state.seq = 0;
-    state.connectionData = []
-    doAssignments();
+    state = { seq: 0, connectionData: [] }
+    setupState()
     state.onConnection = sock.on('connection', (socket) => {
-      
-      let id = state.assignID(socket)
-      let cb = socket.on('message', (type, body) => {
-        state.processMessage(socket, id, type, body);
-      });
-      state.connectionData.push({id, cb});
+      state.connectionData.push({ seq: ++state.seq, socket });
+      processEntriesFrom(state.connectionData.length - 1);
     });
   }
 }
 
-module.exports.deregister = () => {
-  console.log("socket.js deregistering");
-  return state;
+const processEntriesFrom = (n) => {
+  for (i = n; i < state.connectionData.length; i++) {
+    let entry = state.connectionData[i];
+    entry.id = "Session: " + entry.seq;
+    if (entry.cb) {
+      entry.socket.off("message", entry.cb)
+    }
+    entry.cb = entry.socket.on('message', (type, body) => {
+      state.processMessage(entry.socket, entry.id, type, body);
+    });
+  }
 }
+
+
+const path = require("path");
+const fsp = require("fs-promise");
+
+const setupState = () => {
+  const name = "mwolf";
+  state.processMessage = (socket, id, type, data) => {
+    // we tell the client to execute 'new message'
+    console.log("got a message " + type);
+    if (type === "getTodo") {
+      socket.send("id", id);
+      fsp.readFile(path.join(".data", name + ".md")).
+        then(contents => contents.toString())
+        .then(
+        list => socket.send("todo",
+          {
+            id: id,
+            todo: "THIS IS NOT THE list"
+          }));
+    } else if (type === 'getID') {
+      socket.send("id", id);
+    }
+  }
+}
+
+
+
+
+const dumpConnnections = () => {
+  console.log(state.connectionData.length + " connections")
+  state.connectionData.forEach((element, i) => {
+    console.log(`${i}: ${element.id}`)
+  }, this);
+}
+
 
 
