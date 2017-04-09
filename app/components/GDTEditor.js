c8b8d1aea7b0c5e229a2bed28e2cdf8d0bf3c8d1
Mike Wolf <mw@mike-wolf.com>
2017-04-09 15:01:38 -0400

    Get input state and partially process

--- app/components/GDTEditor.js
+++ app/components/GDTEditor.js
@@ -1,6 +1,9 @@
 const React = require('react');
 const CodeMirror = require('react-codemirror');
+import SocketStatus from "./socketStatus";
 require('codemirror/lib/codemirror.css');
+
+
 class GDTEditor extends React.Component{
 	constructor(props) {
 		super(props);
@@ -18,12 +21,40 @@ class GDTEditor extends React.Component{
 	componentWillUnMount(){
 		console.log("Unmounting")
 	}
+	testFunction() {
+		console.log("TESTed")
+	}
+	checkEnter(e) {
+		if(e.which === 13 ){
+			this.setState({code: this.state.code + "XX"})
+			this.readTodos()
+		}
+	}
+	setInput(e){
+		console.log(e)
+	}
+	readTodos() {
+		console.log("READing " + this.textInput.value)
+	}
 	render() {
 		var options = {
 			lineNumbers: true,
 		};
-		console.log("Render")
-		return <CodeMirror value={this.state.code} onChange={this.updateCode.bind(this)} options={options} />
+			// onBlur={this.readTodos.bind(this)}
+		
+		console.log("Rendered once more!!!")
+	
+		return (<div>
+			<input
+			ref={(input)=>{this.textInput=input}}
+			onKeyPress={this.checkEnter.bind(this)}
+			onBlur={this.readTodos.bind(this)}
+			/>
+		<CodeMirror value={this.state.code} onChange={this.updateCode.bind(this)} options={options} />
+        <SocketStatus 
+		
+		/>
+		</div>)
 	}
 };
 
