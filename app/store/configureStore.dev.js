7d8747bcc293babd7efe812c36e865d3138a2ce5
Mike Wolf <mw@mike-wolf.com>
2017-04-11 19:57:43 -0400

    Fix problem in persisting to local storage

--- app/store/configureStore.dev.js
+++ app/store/configureStore.dev.js
@@ -1,20 +1,37 @@
 import rootReducer from '../reducers';
 import DevTools from '../containers/DevTools';
 import {compose, createStore} from 'redux';
-import persistState from 'redux-localstorage'
-const config = {
-    key: "todo"
-};
+import debounce from "redux-localstorage-debounce"
+// import persistState from 'redux-localstorage'
+
+
+import persistState, {mergePersistedState} from 'redux-localstorage';
+import adapter from 'redux-localstorage/lib/adapters/localStorage';
+// import filter from 'redux-localstorage-filter';
+
+// const reducer = compose(
+//   mergePersistedState()
+// )(rootReducer);
+
+const storage = compose(
+  debounce(100),
+//   filter('nested.key')
+)(adapter(window.localStorage));
+
 const enhancer = compose(
   DevTools.instrument(),
-  persistState(undefined, config),
-)
+  /* applyMiddleware(...middlewares) */
+  persistState(storage, 'todo')
+);
+
+
 
 // const store = createStore(/*reducer, [initialState]*/, enhancer)
 export default function configureStore(initialState) {
     const store = createStore(
         rootReducer,
         initialState,
+        // DevTools.instrument()
         enhancer
     );
 
