f387655c22237cea192f1d78c8ebbbb013fe9620
Mike Wolf <mw@mike-wolf.com>
2017-04-12 12:07:50 -0400

    Added persist to local storage

--- app/store/configureStore.dev.js
+++ app/store/configureStore.dev.js
@@ -1,39 +1,30 @@
 import rootReducer from '../reducers';
 import DevTools from '../containers/DevTools';
-import {compose, createStore} from 'redux';
-import debounce from "redux-localstorage-debounce"
+import { compose, createStore } from 'redux';
 // import persistState from 'redux-localstorage'
+import throttle from 'lodash/throttle';
 
 
-import persistState, {mergePersistedState} from 'redux-localstorage';
-import adapter from 'redux-localstorage/lib/adapters/localStorage';
-// import filter from 'redux-localstorage-filter';
+import { loadState, saveState } from './localStorage';
+export default function configureStore() {
 
-// const reducer = compose(
-//   mergePersistedState()
-// )(rootReducer);
+const persistedState = loadState();
+  const store = createStore(
+    rootReducer,
+    persistedState,
+    DevTools.instrument()
+    
+  );
+  // if (module.hot) {
+  //   // Enable Webpack hot module replacement for reducers
+  //   module.hot.accept('../reducers', () => {
+  //     const nextReducer = require('../reducers').default;
+  //     store.replaceReducer(nextReducer);
+  //   });
+  // }
+  store.subscribe(throttle(() => {
+    saveState(store.getState());
+  }, 1000));
 
-const storage = compose(
-  debounce(100),
-//   filter('nested.key')
-)(adapter(window.localStorage));
-
-const enhancer = compose(
-  DevTools.instrument(),
-  /* applyMiddleware(...middlewares) */
-  persistState(storage, 'todo')
-);
-
-
-
-// const store = createStore(/*reducer, [initialState]*/, enhancer)
-export default function configureStore(initialState) {
-    const store = createStore(
-        rootReducer,
-        initialState,
-        // DevTools.instrument()
-        enhancer
-    );
-
-    return store;
+  return store;
 }
